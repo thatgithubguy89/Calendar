1940009ce6c3ac44212a3013fd382c613afec7b8
Jory Adair <joryadair@gmail.com>
2023-10-06 14:55:20 -0400

    add code behind for usereventslist

---
+++ Calendar/Pages/UserCalendar/UserEventsList.razor.cs
@@ -0,0 +1,20 @@
+﻿using Calendar.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Calendar.Pages.UserCalendar
+{
+    public partial class UserEventsList : ComponentBase
+    {
+        [Parameter]
+        public List<UserEvent> UserEvents { get; set; }
+
+        [Parameter]
+        public EventCallback<string> OnDeleteEvent { get; set; }
+
+        // This is used to call the DeleteEvent method on the parent component SingleDay.
+        private async Task DeleteEvent(string id)
+        {
+            await OnDeleteEvent.InvokeAsync(id);
+        }
+    }
+}
